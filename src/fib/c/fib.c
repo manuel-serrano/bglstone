16154acdfef98b1a1986ef4d90ff56431031ca58
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-09-18 13:25:51 +0200

    Add missing Makefile

--- src/fib/c/fib.c
+++ src/fib/c/fib.c
@@ -3,22 +3,23 @@
 
 static long res = 0;
 
-static long fib( long x ) {
-   if( x < 2 ) {
+static long fib(long x) {
+   if (x < 2) {
       return 1;
    } else {
-      return fib( x - 1 ) + fib( x - 2 );
+      return fib(x - 1) + fib(x - 2);
    }
 }
 
-static void doit( int num ) {
-   while( num > 0 ) {
-      res += fib( 30 );
+static void doit(int num) {
+   while (num > 0) {
+      res = fib(35);
       num--;
    }
 }
 
-int main( int argc, char *argv[] ) {
-   doit( 300 );
-   return res == 0;
+int main(int argc, char *argv[]) {
+   doit(900);
+   fprintf(stderr, "%ld\n", res);
+   return res != 14930352;
 }
