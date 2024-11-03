75bed8aff5f1df9ae138640b82096e65d88f2699
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-11-03 14:06:52 +0100

    r7rs-nucleic

---
+++ src/r7rs-nucleic/bigloo/r7rs-nucleic.scm
@@ -0,0 +1,7 @@
+(module r7rs-nucleic
+   (include "../../r7rs/bigloo.sch")
+   (include "../../r7rs/src/nucleic.scm")
+   (main main))
+
+(define (default-input) "../../r7rs/inputs/nucleic.input")
+(define (pmem) "5 () 33.797594890762724")
