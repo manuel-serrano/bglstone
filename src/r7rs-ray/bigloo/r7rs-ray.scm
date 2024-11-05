9b32e186fcd19a87406ea11144d95fe8fff94603
Manuel Serrano <Manuel.Serrano@inria.fr>
2024-11-05 10:35:39 +0100

    More fixes

--- src/r7rs-ray/bigloo/r7rs-ray.scm
+++ src/r7rs-ray/bigloo/r7rs-ray.scm
@@ -3,5 +3,15 @@
    (include "../../r7rs/src/ray.scm")
    (main main))
 
+(make-directories "outputs")
+
 (define (default-input) "../../r7rs/inputs/ray.input")
 (define (pmem) "5 1 \"outputs/ray.output\" ok")
+
+(register-exit-function!
+   (lambda (r)
+      (let ((dir "outputs"))
+	 (for-each (lambda (f)
+		      (delete-file (make-file-name dir f)))
+	    (directory->list dir))
+	 (delete-directory "outputs"))))
